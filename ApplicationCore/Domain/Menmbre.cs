2e9bf0d76bd92cca6a6c866278ee196a0986554d
Dina <100855498+GharbiDina@users.noreply.github.com>
2024-12-31 18:09:18 +0100

    Entite et association

---
+++ ApplicationCore/Domain/Menmbre.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Domain
+{
+    public class Menmbre
+
+    {
+        public string matricule { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public virtual IList<Tache> Taches { get; set; }
+
+
+
+    }
+}
---
+++ ApplicationCore/Domain/Projet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationCore.Domain
+{
+    public class Projet
+    {
+        public string Code { get; set; }
+        public string Titre { get; set; }
+        public string Description { get; set; }
+    }
+}
